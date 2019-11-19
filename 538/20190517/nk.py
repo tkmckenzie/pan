5d54b13647167fd1cb5794067c3d3d00511ea753
tmcken <tmcken@sandia.gov>
2019-11-19 07:14:33 -0700

    NA:

--- 538/20190517/nk.py
+++ 538/20190517/nk.py
@@ -3,8 +3,8 @@ import numpy as np
 p = 0.5
 target = 0.5
 
-max_D = 10000
-max_L = 10000
+max_D = 4
+max_L = 4
 
 # p_array[D, L] = prob L will win given armies of size D and L
 p_array = np.zeros((max_D + max_L + 1, max_L + 1))
@@ -19,4 +19,17 @@ for L in range(2, max_L + 1):
 		p_array[D, L] = p * p_array[D - 1, L] + (1 - p) * p_array[D + 1, L - 1]
 p_array = p_array[:max_D + 1, :max_L + 1]
 
-print(np.where(p_array == 0.5))
+print(p_array)
+
+asdf
+# Alternate method
+p_array = np.zeros((max_D + max_L + 1, max_L + 1))
+
+p_array[0,:] = 1
+p_array[:,0] = 0
+
+p_array[:,1] = p**np.arange(max_D + max_L + 1)
+
+for L in range(2, max_L + 1):
+	p_array[:,L] = (1 - p) * np.concatenate((p_array[1:,L-1], np.array([0])))
+	
